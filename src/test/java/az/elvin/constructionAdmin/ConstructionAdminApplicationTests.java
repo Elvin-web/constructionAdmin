bd9b758ea7a6a6b8410fd2080f7fb35b7582ad63
Elvin Yusifli <72-eyusifli@users.noreply.https>
2022-01-01 18:39:15 +0400

    3

--- src/test/java/az/elvin/constructionAdmin/ConstructionAdminApplicationTests.java
+++ src/test/java/az/elvin/constructionAdmin/ConstructionAdminApplicationTests.java
@@ -2,11 +2,11 @@ package az.elvin.constructionAdmin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-@SpringBootTest
-public class ConstructionAdminApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-}
+//
+//@SpringBootTest
+//public class ConstructionAdminApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//}
